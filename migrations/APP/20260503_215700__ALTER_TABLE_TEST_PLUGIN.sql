4384d8b851a114416d40dfaa50cb9eedb5bfa0ba
wildanaizzaddin <wildanaizzaddin@local>
2026-05-03 21:57:00 +0700

    [ALTER TABLE] APP.TEST_PLUGIN — wildanaizzaddin @ 2026-05-03T21:57:00.273129

---
+++ migrations/APP/20260503_215700__ALTER_TABLE_TEST_PLUGIN.sql
@@ -0,0 +1,7 @@
+-- DDL Change Tracker
+-- Timestamp  : 2026-05-03T21:57:00
+-- User       : wildanaizzaddin
+-- Datasource : 43.156.27.134:31214
+-- Schema     : APP
+
+alter table test_plugin drop column created_by
