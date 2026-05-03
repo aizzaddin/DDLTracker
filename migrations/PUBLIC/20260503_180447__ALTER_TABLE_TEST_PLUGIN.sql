026c3eccbf2957e83b1530a2532e66726d4a79d5
wildanaizzaddin <wildanaizzaddin@local>
2026-05-03 18:04:47 +0700

    [ALTER TABLE] PUBLIC.TEST_PLUGIN — wildanaizzaddin @ 2026-05-03T18:04:47.053493

---
+++ migrations/PUBLIC/20260503_180447__ALTER_TABLE_TEST_PLUGIN.sql
@@ -0,0 +1,7 @@
+-- DDL Change Tracker
+-- Timestamp  : 2026-05-03T18:04:47
+-- User       : wildanaizzaddin
+-- Datasource : refactoring
+-- Schema     : PUBLIC
+
+alter table public.test_plugin add user_id uuid
